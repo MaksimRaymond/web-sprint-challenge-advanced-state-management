84bfeb2f8b6d7eead7e9283aa8b4087feab16a3d
Maksim Raymond <maksim-raymond@lambdastudents.com>
2021-02-19 16:58:03 -0800

    actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,35 @@
 import axios from 'axios';
+export const FETCH_SMURF_START = " FETCH_SMURF_START ";
+export const FETCH_SMURF_SUCCESS = " FETCH_SMURF_SUCCESS";
+export const FETCH_SMURF_FAIL = "FETCH_SMURF_FAIL";
+export const UPDATE_ERROR_MESSAGE = "UPDATE_ERROR_MESSAGE";
+export const ADD_NEW_SMURF = "ADD_NEW_SMURF";
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+export const fetchSmurfs = () => dispatch => {
+    dispatch({ type: FETCH_SMURF_START });
+    axios 
+        .get("http://localhost:3333/smurfs")
+        .then((res) => {
+            dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data});
+        })
+        .catch(err => {
+            dispatch({ type: FETCH_SMURF_FAIL, payload: err.response.data });
+        });
+}
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
+export const addSmurf = (updatedSmurfs) => dispatch => {
+    axios.post("http://localhost:3333/smurfs", updatedSmurfs)
+    .then((res) => {
+        dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+        dispatch({ type: FETCH_SMURF_FAIL, payload: err.response.data });
+    });
+}
 //3. Add a standard action that allows us to set the value of the error message slice of state.
+
+export const updateError = () => dispatch => {
+    dispatch({ type: UPDATE_ERROR_MESSAGE });
+}
